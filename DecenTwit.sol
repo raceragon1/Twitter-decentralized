f6315c76b17a6f26b01c88e8a709eee7eabd5ab4
"raceragon1" <"raceragon1@gmail.com">
2022-01-24 00:49:06 +0530

    tweet is done, tweet number is done, get tweet is not

--- DecenTwit.sol
+++ DecenTwit.sol
@@ -7,15 +7,19 @@ contract decenTwitter{
     }
 
     mapping(address=>mapping(int => allTweets)) public TweetDatabase;
+
+    event Tweeted(string value);
+    event Tweetnumber(int value);
     
     int i=0;
-    //function tweet(uint tweetnumber, string memory Tweet) public {
     function tweet(string memory Tweet) public {
         i=i+1;
         TweetDatabase[msg.sender][i] = allTweets(Tweet);
+        emit Tweeted(Tweet);
+        emit Tweetnumber(i);
     }
 
-    function gettweet(int tweetnumber) public returns(struct){
+    function gettweet(int tweetnumber) public returns(string memory){
         return TweetDatabase[msg.sender][tweetnumber];
     }
 
