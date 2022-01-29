22d3fd52f2877804138c4752c58bdde67af64916
"raceragon1" <"raceragon1@gmail.com">
2022-01-29 14:26:02 +0530

    trying somthing different

--- DecenTwit.sol
+++ DecenTwit.sol
@@ -2,24 +2,32 @@
 pragma solidity ^0.8.1;
 contract decenTwitter{
 
-    struct allTweets {
-        string tweets;
+    struct Tweets {
+         string tweet;
     }
 
-    mapping(address=>mapping(uint => allTweets)) TweetDatabase;
+    mapping(address => Tweets) TweetDatabase;
 
     event Tweeted(string value);
     event Tweetnumber(uint value);
     
     
-    function tweet(string memory Tweet, uint _tweetnumber) public {
-        TweetDatabase[msg.sender][_tweetnumber] = allTweets(Tweet);
-        emit Tweeted(Tweet);
+    function tweet(string memory _tweet, uint _tweetnumber) public {
+        _tweetnumber = Tweets(_tweet);
+        TweetDatabase[msg.sender][_tweetnumber] = Tweets(_tweet);
+        emit Tweeted(_tweet);
         emit Tweetnumber(_tweetnumber);
     }
 
+    //function tweet(string memory _tweet, uint _tweetnumber) public {
+    //    _tweetnumber = Tweets(_tweet);
+    //    emit Tweeted(_tweet);
+    //    emit Tweetnumber(_tweetnumber);
+    //}
+    
+
     function gettweet(uint _tweetnumber) public returns(string memory){
-        return TweetDatabase[msg.sender][_tweetnumber];
+        return (_tweetnumber.tweet);
     }
 
 }
