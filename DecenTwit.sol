781865faca5535a570b3d6fc1fdc5be591c4c09f
"raceragon1" <"raceragon1@gmail.com">
2022-01-29 14:41:59 +0530

    Done with double mapping

--- DecenTwit.sol
+++ DecenTwit.sol
@@ -6,28 +6,26 @@ contract decenTwitter{
          string tweet;
     }
 
-    mapping(address => Tweets) TweetDatabase;
+    mapping(address => mapping(uint => Tweets)) TweetDatabase;
+   
 
     event Tweeted(string value);
     event Tweetnumber(uint value);
     
     
     function tweet(string memory _tweet, uint _tweetnumber) public {
-        _tweetnumber = Tweets(_tweet);
         TweetDatabase[msg.sender][_tweetnumber] = Tweets(_tweet);
+
+        //TweetDatabase[msg.sender][_tweetnumber] = Tweets(_tweet);
+
         emit Tweeted(_tweet);
         emit Tweetnumber(_tweetnumber);
     }
 
-    //function tweet(string memory _tweet, uint _tweetnumber) public {
-    //    _tweetnumber = Tweets(_tweet);
-    //    emit Tweeted(_tweet);
-    //    emit Tweetnumber(_tweetnumber);
-    //}
-    
+   
 
-    function gettweet(uint _tweetnumber) public returns(string memory){
-        return (_tweetnumber.tweet);
+    function gettweet(uint _tweetnumber) public view returns(string memory){
+        return (TweetDatabase[msg.sender][_tweetnumber].tweet);
     }
 
 }
